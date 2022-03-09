7d4e9dbc5c745e9cd75e414fbe85c8245965ad14
kimberly-waf <30293069+kimberly-waf@users.noreply.github.com>
2022-03-09 14:02:10 -0600

    Create errors.php

---
+++ errors.php
@@ -0,0 +1,5 @@
+<?php 
+
+Notice: Undefined index: shared_id in /home/wafinter/subdomains/wp.wafisherinteractive.com/burnsville-fire-muster/wp-content/plugins/gravityformsppcp/includes/class-gf-ppcp-api.php on line 196
+
+Notice: Undefined index: auth_code in /home/wafinter/subdomains/wp.wafisherinteractive.com/burnsville-fire-muster/wp-content/plugins/gravityformsppcp/includes/class-gf-ppcp-api.php on line 201
